3e3194aa20a8d7c5752124b3d54d8c4d9ce990ab
evalambert <145194730+evalambert@users.noreply.github.com>
2026-01-05 17:10:19 +0100

    breakpoint

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,29 @@
 export default ({ env }) => ({
-    'users-permissions': {
-        config: {
-            jwtSecret: env('JWT_SECRET'),
-        },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
     },
-    upload: {
-        config: {
-            provider: 'cloudinary',
-            providerOptions: {
-                cloud_name: env('CLOUDINARY_NAME'),
-                api_key: env('CLOUDINARY_KEY'),
-                api_secret: env('CLOUDINARY_SECRET'),
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
-        },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+      breakpoints: {
+        xlarge: 1920,
+        large: 1200,
+        medium: 750,
+        small: 500,
+        thumbnail: 150,
+      },
     },
+  },
 });
